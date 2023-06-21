9041abf42f2dead896db4dc53222d920d5bd6d80
Rixtinator <129871232+Rixtinator@users.noreply.github.com>
2023-06-21 13:48:05 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
+import { RouterModule } from '@angular/router';
+import { allAppRoutes } from './routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(allAppRoutes),
     AppRoutingModule
   ],
   providers: [],
