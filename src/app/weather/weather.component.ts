93ab20903c96b5edfd50ee20dbc441984ccb432d
Rixtinator <129871232+Rixtinator@users.noreply.github.com>
2023-06-21 20:54:49 +0200

    add apixuService to get data to console

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApixuService } from "../apixu.service";
 
 @Component({
   selector: 'app-weather',
@@ -8,7 +9,15 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class WeatherComponent {
   public weatherSearchForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) { this.weatherSearchForm = this.formBuilder.group({ location: [''] }); } ngOnInit(): void { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private apixuService: ApixuService) { this.weatherSearchForm = this.formBuilder.group({ location: [''] }); };
+  ngOnInit(): void { };
+  sendToAPIXU(formValues) {
+    this.apixuService
+      .getWeather(formValues.location)
+      .subscribe(data => console.log(data));
+  };
 }
 
 
