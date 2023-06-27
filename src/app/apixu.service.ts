9e8994d3cb5b160be6ea52ae760c78e6b4680698
Rixtinator <129871232+Rixtinator@users.noreply.github.com>
2023-06-27 08:57:54 +0200

    add working api link

--- src/app/apixu.service.ts
+++ src/app/apixu.service.ts
@@ -11,7 +11,7 @@ export class ApixuService {
 
   getWeather(location) {
     return this.http.get(
-      `http://api.weatherstack.com/current?access_key=33910305b031c4cda815cf064293cb5d&query=` + location
+      `http://api.weatherapi.com/v1/current.json?key=94d293e56a01480281863536232706&q=` + location + `&aqi=no`
     );
   }
 }
